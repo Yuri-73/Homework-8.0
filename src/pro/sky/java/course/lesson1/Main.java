5f531818c6d9e96d9b21830a986f47519b65f57e
Юрий <fedejkin@gmail.com>
2023-03-15 11:00:39 +0300

    public variables

--- src/pro/sky/java/course/lesson1/Main.java
+++ src/pro/sky/java/course/lesson1/Main.java
@@ -2,7 +2,18 @@ package pro.sky.java.course.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Author bookAuthorTolstoy = new Author("Лев", "Толстой");
+        Book book = new Book(bookAuthorTolstoy, "Война и мир", 1982);
+        System.out.println("book.authorName = " + book.authorName.firstName + " " + book.authorName.lastName);
+        System.out.println("book.bookName = " + book.bookName);
+        System.out.println("book.publishingYear = " + book.publishingYear);
+
+        System.out.println();
+        Author bookAuthorSimonov = new Author("Константин", "Симонов");
+        Book book2 = new Book(bookAuthorSimonov, "Живые и мёртвые", 1995);
+        System.out.println("book.authorName = " + book2.authorName.firstName + " " + book2.authorName.lastName);
+        System.out.println("book.bookName = " + book2.bookName);
+        System.out.println("book.publishingYear = " + book2.publishingYear);
     }
 }
 
